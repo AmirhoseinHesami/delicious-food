5605244ec00a5665b61abe633340b1d039ff5edb
AmirhoseinHesami <amirhosein1383hesami@gmail.com>
2023-06-08 03:19:22 +0330

    add animation to Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,19 @@
 import React from "react";
 import Popular from "../components/Popular";
 import Veggie from "../components/Veggie";
+import { motion } from "framer-motion";
 
 function Home() {
   return (
-    <div>
+    <motion.div
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+    >
       <Veggie />
       <Popular />
-    </div>
+    </motion.div>
   );
 }
 
