853882a620b27556dffcde725824d5cfc09928e0
AmirhoseinHesami <amirhosein1383hesami@gmail.com>
2023-06-08 03:22:10 +0330

    implemented cuisine and pages

---
+++ src/pages/Cuisine.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import { Link, useParams } from "react-router-dom";
+
+function Cuisine() {
+  const [cuisine, setCuisine] = useState([]);
+  let params = useParams();
+
+  const getCuisine = async (name) => {
+    const data = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}`
+    );
+    const recipes = await data.json();
+    setCuisine(recipes.results);
+  };
+
+  useEffect(() => {
+    getCuisine(params.type);
+  }, [params.type]);
+
+  return (
+    <Grid
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      {cuisine.map((item) => {
+        return (
+          <Card key={item.id}>
+            <Link to={"/recipe/" + item.id}>
+              <img src={item.image} alt="" />
+              <h4>{item.title}</h4>
+            </Link>
+          </Card>
+        );
+      })}
+    </Grid>
+  );
+}
+
+const Grid = styled(motion.div)`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(17rem, 1fr));
+  grid-gap: 2rem;
+  margin-top: 2rem;
+`;
+
+const Card = styled.div`
+  img {
+    width: 100%;
+    border-radius: 2rem;
+  }
+  a {
+    text-decoration: none;
+  }
+  h4 {
+    text-align: center;
+    padding: 0.5rem;
+    font-size: 20px;
+  }
+`;
+
+export default Cuisine;
--- src/pages/Pages.js
+++ src/pages/Pages.js
@@ -1,11 +1,22 @@
 import React from "react";
 import Home from "./Home";
+import Cuisine from "./Cuisine";
+import Searched from "./Searched";
+import Recipe from "./Recipe";
+import { Route, Routes, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 
 function Pages() {
+  const location = useLocation();
   return (
-    <div>
-      <Home />
-    </div>
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Home />} />
+        <Route path="/cuisine/:type" element={<Cuisine />} />
+        <Route path="/searched/:search" element={<Searched />} />
+        <Route path="/recipe/:name" element={<Recipe />} />
+      </Routes>
+    </AnimatePresence>
   );
 }
 
