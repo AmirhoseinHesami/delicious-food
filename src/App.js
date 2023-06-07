b2aaf8d8eb575e7f4ffc724821550aa99436697e
AmirhoseinHesami <amirhosein1383hesami@gmail.com>
2023-06-08 03:26:03 +0330

    put all components together in app.js and make the navigation happen also styled the app with styled components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
 import Pages from "./pages/Pages";
+import Category from "./components/Category";
+import { BrowserRouter, Link } from "react-router-dom";
+import Search from "./components/Search";
+import styled from "styled-components";
+import { devices } from "./size";
+import { GiKnifeFork } from "react-icons/gi";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
-      <Pages />
+      <BrowserRouter>
+        <Nav>
+          <GiKnifeFork />
+          <Logo to={"/"}>Delicious</Logo>
+        </Nav>
+        <Search />
+        <Category />
+        <Pages />
+      </BrowserRouter>
     </div>
   );
 }
+
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 400;
+  font-family: "Lobster Two", cursive;
+
+  @media ${devices.tablet} {
+    font-size: 2rem;
+  }
+`;
+
+const Nav = styled.div`
+  padding: 0rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+
+  svg {
+    font-size: 2rem;
+  }
+
+  @media ${devices.tablet} {
+    svg {
+      font-size: 3rem;
+    }
+  }
+`;
 
 export default App;
